3fff4f193b198e35c582d5f8a0f31bce5b7c054e
arda.isgoren <arda.isgoren@flo.com.tr>
2021-08-24 08:30:52 +0300

    stg_customer, stg_sales_order_line oluşuturuldu

---
+++ first_project/models/staging/stg_customer.sql
@@ -0,0 +1,11 @@
+SELECT
+	entity_id as id,
+	created_at,
+	updated_at,
+	firstname as name,
+	middle_name,
+	lastname as last_name,
+	dob,
+	gender,
+	is_active
+FROM customer_entity
---
+++ first_project/models/staging/stg_sales_order_line.sql
@@ -0,0 +1,16 @@
+SELECT
+	company,
+	store_id,
+	item_id,
+	order_id,
+	created_at,
+	status
+	increment_id,
+	sku,
+	--discount order_batches ana verisinde mevcut dict �eklinde
+	qty_ordered,
+	price,
+	base_cost,
+	tax_percent,
+	discount_amount
+FROM sales_order_item
